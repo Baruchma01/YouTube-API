2c075eddad2a8c2805f15b0b1d60ed834ffa1470
Baruch Mashasha <baruchma01@gmail.com>
2020-07-22 15:38:53 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import SearchPage from './components/search-page/SearchPage';
-import ItemPage from './components/item-page/ItemPage';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import React from "react";
+import SearchPage from "./components/search-page/SearchPage";
+import ItemPage from "./components/item-page/ItemPage";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container" style={{minHeight:'1030px'}}>
+    <div className="container" style={{ minHeight: "1030px" }}>
       <Router>
-      <div>
-        <SearchPage />
-        <Switch>
-          {/* <Route exact path="/" component={SearchPage}/> */}
-          <Route exact path="/item-page" component={ItemPage}/>
-        </Switch>
-      </div>
-    </Router>
+        <div>
+          <SearchPage />
+          <Switch>
+            {/* <Route exact path="/" component={SearchPage}/> */}
+            <Route exact path="/item-page" component={ItemPage} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
